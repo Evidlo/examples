96ba000b52b62962ee65b9e2a74abb3fcb05b617
evan <evan@evanw.org>
2017-04-02 19:45:08 -0400

    added fib

---
+++ python/fibonacci.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+def fibonacci(start=0):
+    a = 0
+    b = 1
+    c = 0
+    iteration = 0
+
+    while True:
+        if iteration >= start:
+            yield c
+
+        a = b
+        b = c
+        c = a + b
+
+        iteration += 1
+
+# get the 20th fibonacci number
+print(fibonacci(20).next())
+
+fib = fibonacci(10)
+
+# get the 10th fibonacci number
+print(fib.next())
+
+# get the 11th fibonacci number
+print(fib.next())
+
+# get the 12th - 15th number as list
+print([fib.next() for _ in range(4)])
+
