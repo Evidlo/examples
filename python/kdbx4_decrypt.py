fac13f47111cd86056f180d2333bae72b0796c38
evan <evan@evanw.org>
2018-05-17 06:38:34 -0500

    got aes kdf working

--- python/kdbx4_decrypt.py
+++ python/kdbx4_decrypt.py
@@ -39,6 +39,7 @@ header_item_ids = {0: 'end',
                    11: 'kdf_parameters'
 }
 
+
 # read dynamic header
 
 # offset of first header byte
@@ -47,20 +48,28 @@ offset = 12
 header = {}
 
 # loop until end of header
-while b[offset] != 0:
+while True:
+    # read item id (1 byte)
+    item_id = header_item_ids[b[offset]]
+    offset += 1
     # read size of item (4 bytes)
-    size, = struct.unpack('<I', b[offset + 1:offset + 5])
-    # insert item into header dict
-    header[header_item_ids[b[offset]]] = b[offset + 5:offset + 5 + size]
-    # move to next header item
-    # (1 byte for header item id, 4 bytes for item size, `size` bytes for data)
-    offset += 1 + 4 + size
+    size, = struct.unpack('<I', b[offset:offset + 4])
+    offset += 4
+    if item_id == 'end':
+        offset += size
+        break
+    else:
+        # insert item into header dict
+        header[item_id] = b[offset:offset + size]
+        offset += size
 
-# move from `end` to start of header hash
-size, = struct.unpack('<I', b[offset + 1:offset + 5])
-offset += 1 + 4 + size
+header_end = offset
 
-header_hash = hashlib.sha256(b[:offset]).digest()
+header_sha256_hash = b[offset:offset + 32]
+assert header_sha256_hash == hashlib.sha256(b[:header_end]).digest(), "Header verification failed"
+offset += 32
+
+header_hmac_hash = b[offset:offset + 32]
 offset += 32
 
 # read kdf parameters
@@ -83,14 +92,22 @@ value_types = {
 
 kdf_offset += 2
 while header['kdf_parameters'][kdf_offset] != 0:
+
+    # read type of item (1 bytes)
     value_type = header['kdf_parameters'][kdf_offset]
-    key_size, = struct.unpack('<I', header['kdf_parameters'][kdf_offset + 1:kdf_offset + 5])
-    key = header['kdf_parameters'][kdf_offset + 5:kdf_offset + 5 + key_size]
-    kdf_offset += 1 + 4 + key_size
+    kdf_offset += 1
+    # read size of item key
+    key_size, = struct.unpack('<I', header['kdf_parameters'][kdf_offset:kdf_offset + 4])
+    kdf_offset += 4
+    # read item key (`key_size` bytes)
+    key = header['kdf_parameters'][kdf_offset:kdf_offset + key_size]
+    kdf_offset += key_size
+    # read item value size (4 bytes)
     value_size, = struct.unpack('<I', header['kdf_parameters'][kdf_offset:kdf_offset + 4])
+    kdf_offset += 4
     value, = struct.unpack('<' + value_types[value_type].format(length=value_size),
-                          header['kdf_parameters'][kdf_offset + 4:kdf_offset + 4 + value_size])
-    kdf_offset += 4 + value_size
+                          header['kdf_parameters'][kdf_offset:kdf_offset + value_size])
+    kdf_offset += value_size
 
     kdf_parameters[key.decode('utf-8')] = value
 
@@ -104,6 +121,8 @@ kdf_uuids = {
 
 from pygcrypt.ciphers import Cipher
 from pygcrypt.context import Context
+import hmac
+import argon2
 
 # hash the password
 if password:
@@ -125,7 +144,14 @@ else:
 key_composite = hashlib.sha256(password_composite + keyfile_composite).digest()
 
 if kdf_parameters['$UUID'] == kdf_uuids['argon2']:
-    pass
+    transformed_key = argon2.low_level.hash_secret(key_composite,
+                                                       kdf_parameters['S'],
+                                                       hash_len=256,
+                                                       type=argon2.low_level.Type.D,
+                                                       time_cost=kdf_parameters['I'],
+                                                       memory_cost=kdf_parameters['M'] // 1000,
+                                                       parallelism=kdf_parameters['P'],
+    )
 elif kdf_parameters['$UUID'] == kdf_uuids['aes']:
     # set up a context for AES128-ECB encryption to find transformed_key
     context = Context()
@@ -140,26 +166,92 @@ elif kdf_parameters['$UUID'] == kdf_uuids['aes']:
     for _ in range(0, rounds):
         transformed_key = context.cipher.encrypt(transformed_key)
 
-    # combine the transformed key with the header master seed to find the master_key
     transformed_key = hashlib.sha256(transformed_key).digest()
-    master_key = hashlib.sha256(bytes(header['master_seed']) + transformed_key).digest()
 else:
     raise Exception('Unsupported key derivation method')
 
+# combine the transformed key with the header master seed to find the master_key
+master_key = hashlib.sha256(bytes(header['master_seed']) + transformed_key).digest()
 
-# # dict containing inner header items
-# inner_header = {}
+# validate second header hash
+hmac_key = hashlib.sha512(b'\xff' * 8 + hashlib.sha512(header['master_seed'] + transformed_key + b'\x01').digest()).digest()
+assert header_hmac_hash == hmac.new(hmac_key, b[:header_end], hashlib.sha256).digest(), "Header validation failed"
 
-# # inner heaer item lookup table
-# inner_header_item_ids = {0: 'end',
-#                          1: 'inner_random_stream_id',
-#                          2: 'inner_random_stream_key',
-#                          3: 'flags'
-#                          }
+# ---------- Process and verify payload blocks ----------
 
-# while b[offset] != 0:
-#     # read size of item (4 bytes)
-#     size, = struct.unpack('<I', b[offset + 1:offset + 5])
-#     inner_header[inner_header_item_ids[offset]] = b[offset + 5:offset + 5 + size]
-#     offset += 1 + 4 + size
+payload_data = b''
 
+# read payload block data, block by block
+while True:
+    # read hmac hash of payload data block (32 bytes)
+    block_hmac_hash = b[offset:offset + 32]
+    # read block size (4 bytes)
+    block_size, = struct.unpack('<I', b[offset + 32:offset + 36])
+    if block_size == 0:
+        break
+    # read block data (`block_size` bytes)
+    block_data = b[offset + 36:offset + 36 + block_size]
+    payload_data += block_data
+
+    offset += 36 + block_size
+
+payload_ciphers = {
+    'aes256': b'1\xc1\xf2\xe6\xbfqCP\xbeX\x05!j\xfcZ\xff',
+}
+
+# ---------- Decrypt payload ----------
+
+if header['cipher_id'] == payload_ciphers['aes256']:
+    # set up a context for AES128-CBC decryption to find the decrypted payload
+    context = Context()
+    cipher = Cipher(b'AES', u'CBC')
+    context.cipher = cipher
+    context.key = master_key
+    context.iv = header['encryption_iv']
+    payload_data = context.cipher.decrypt(payload_data)
+else:
+    raise Exception('Unsupported payload cipher')
+
+import zlib
+
+# check if payload_data is compressed
+if struct.unpack('<I', header['compression_flags']):
+    # decompress using gzip
+    payload_data = zlib.decompress(payload_data, 16 + 15)
+else:
+    payload_data = payload_data
+
+# ---------- Inner header ----------
+
+# dict containing inner header items
+inner_header = {}
+
+# inner header item lookup table
+inner_header_item_ids = {
+    0x00: 'end',
+    0x01: 'inner_random_stream_id',
+    0x02: 'inner_random_stream_key',
+    0x03: 'flags'
+}
+
+inner_offset = 0
+
+# read inner header
+while True:
+    # read item id (1 byte)
+    item_id = inner_header_item_ids[payload_data[inner_offset]]
+    inner_offset += 1
+    # read size of item (4 bytes)
+    size, = struct.unpack('<I', payload_data[inner_offset:inner_offset + 4])
+    inner_offset += 4
+    if item_id == 'end':
+        inner_offset += size
+        break
+    else:
+        inner_header[item_id] = b[inner_offset:inner_offset + size]
+        inner_offset += size
+
+# xml_data immediately follows inner header
+xml_data = payload_data[inner_offset:]
+
+print(xml_data)
