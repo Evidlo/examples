46d03b75ae51c4f85b3dab718c40fb8d9cc72057
evan <evan@evanw.org>
2021-08-01 23:33:06 -0500

    add scroll

--- python/argcomplete_ex.py
+++ python/argcomplete_ex.py
@@ -4,29 +4,16 @@ from getpass import getpass
 import argcomplete, argparse
 
 def path_completer(prefix, parsed_args, **kwargs):
-    f = open('/tmp/log', 'w')
-    f.write('start\n')
+    password = getpass('\nInput Password:')
 
-    # x = input('input:')
-    x = getpass('input:')
-
-    f.write('got input\n')
-    # database = open_databases(**vars(args))
-    f.write('db open')
-    return ['test', 'foo', 'bar']
-
-def move(args):
-    print(args)
+    if password == 'password':
+        return ['helloworld', 'foobar']
 
 parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers()
 
-mv_parser = subparsers.add_parser('mv', help='move a thing')
-patharg = mv_parser.add_argument('path', metavar='PATH', type=str, help='entry/group path')
+patharg = parser.add_argument('path', metavar='PATH', type=str, help='item path')
 patharg.completer = path_completer
-mv_parser.set_defaults(func=move)
 
-print('ran program')
 argcomplete.autocomplete(parser)
-
 args = parser.parse_args()
+print(f'selected item: {args.path}')
